0e3632b71df1c93f4c59fc9cfb13569d5cf8a617
unknown <albert507@naver.com>
2019-11-20 02:44:25 +0900

    make accounts/urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from accounts.views import *
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('sign_up/', SignUpView.as_view(), name='sign_up'),
+    path('sign_in/', SignInView.as_view(), name='sign_in'),
+    path('sign_out/', SignOutView.as_view(), name='sign_out'),
+]
