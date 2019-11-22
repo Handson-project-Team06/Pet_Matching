7b30f66692ecb37cc9da65be3ef54aed9e3884ff
윤연경 <amyss0907@gmail.com>
2019-11-22 15:13:12 +0900

    반려동물모델만듬

--- matching/models.py
+++ matching/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pets(models.Model):
+    objects=models.Manager()
+    name=models.CharField(max_length=30)
+    #pic=models.ImageField(blank=False,)
+    #owner=models.ForeignKey(User)
+
+    def __str__(self):
+        return self.name
--- pet_matching/urls.py
+++ pet_matching/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path,include
+import matching.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',matching.views.home,name = "home"),
+    #path('accounts/',include('accounts.urls',namespace='account')),
 ]
