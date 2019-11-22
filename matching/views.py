82b76d727b506018014a5930eae938927f1657c7
윤연경 <amyss0907@gmail.com>
2019-11-23 01:07:52 +0900

    게시판

--- matching/views.py
+++ matching/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from matching.models import Pet
+from matching.filters import PetFilter
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    pet_list=Pet.objects.all()
+    pet_filter = PetFilter(request.GET,queryset=pet_list)
+    return render(request,'home.html',{'filter':pet_filter})
+
