3ba47db662c3160652dc7b656d8585afdd78ccc1
minjoong <albert507@naver.com>
2019-11-26 22:29:20 +0900

    edit forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -60,8 +60,6 @@ class UserCreationForm(forms.ModelForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password1"])
-        user.is_staff = True
-        user.is_superuser = True
         if commit:
             user.save()
         return user
