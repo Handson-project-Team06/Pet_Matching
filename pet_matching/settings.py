910dbcc45692c21328858d35fd5405c92c9756d1
minjoong <albert507@naver.com>
2019-11-26 22:30:25 +0900

    edit settings

--- pet_matching/settings.py
+++ pet_matching/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'pet_matching.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
