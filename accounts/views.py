c4352ba17fc020ea1a275373bb6e2c5ab5ba517b
unknown <albert507@naver.com>
2019-11-26 14:11:25 +0900

    modify accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,10 @@ from django.utils.decorators import method_decorator
 from django.views import generic
 from django.views.decorators.cache import never_cache
 from django.shortcuts import render
-from accounts.forms import UserCreationForm
+from .forms import UserCreationForm
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.views import LoginView
-
-# todo: 임시 홈뷰
-
+from matching.models import *
 
 class HomeView(generic.TemplateView):
     template_name = 'home.html'
@@ -18,13 +16,15 @@ class SignUpView(generic.CreateView):
     template_name = 'sign_up.html'
     success_url = reverse_lazy('accounts:sign_in')
 
+    def post(self, request, *args, **kwargs):
+        return super(SignUpView, self).post(request, *args, **kwargs)
 
 class SignInView(LoginView):
     template_name = 'sign_in.html'
 
-    # def get_success_url(self):
-    #     #매칭 앱 url로 연결
-    #     return reverse_lazy('')
+    def get_success_url(self):
+        #매칭 앱 url로 연결
+        return reverse_lazy('matching:home')
 
 class SignOutView(generic.RedirectView):
     url = reverse_lazy('accounts:home')
