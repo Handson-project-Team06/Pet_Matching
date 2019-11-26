5ae382eefe2f12c14d469b6ff60385d868c3042c
unknown <albert507@naver.com>
2019-11-26 15:29:47 +0900

    modify models.py add address attribute

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,4 @@ class User(AbstractUser):
     phone_number = models.CharField('핸드폰 번호', max_length=13)
     birth_date = models.DateField('생년월일')
     name = models.CharField('이름', max_length=15)
+    address = models.CharField('주소', max_length=20)
