84bca9b4ff8891826845d78ad391d8741c4ca321
minjoong <albert507@naver.com>
2019-11-26 22:29:27 +0900

    edit urls

--- matching/urls.py
+++ matching/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from .views import *
+from matching.views import *
 
 
 app_name = 'matching'
 
 urlpatterns = [
-   path('home/', home, name='home'),
+   path('home/', PetListView.as_view(), name='home'),
+   path('home/my_pet_list/', MyPetListView.as_view(), name='my_pet_list'),
+   path('home/my_pet_list/pet_create/', PetCreateView.as_view(), name='pet_create'),
+   path('home/my_pet_list/<int:pk>/', PetUpdateView.as_view(), name='pet_update'),
+   path('home/my_pet_list/<int:pk>/delete/', PetDeleteView.as_view(), name='pet_delete'),
+
 ]
--- pet_matching/urls.py
+++ pet_matching/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path, include
 
 urlpatterns = [
     path('matching/', include('matching.urls', namespace='matching')),
@@ -26,3 +27,5 @@ urlpatterns = [
     path('', include('accounts.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
