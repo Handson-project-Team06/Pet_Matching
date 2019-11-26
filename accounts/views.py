7fcb3853c57cd21969e0d03874e9a718bc22fac0
unknown <albert507@naver.com>
2019-11-26 15:03:33 +0900

    modify accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -9,18 +9,21 @@ from django.contrib.auth.views import LoginView
 from matching.models import *
 
 class HomeView(generic.TemplateView):
-    template_name = 'home.html'
+    template_name = 'accounts/home.html'
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    template_name = 'sign_up.html'
-    success_url = reverse_lazy('accounts:sign_in')
+    template_name = 'accounts/sign_up.html'
+    # success_url = reverse_lazy('accounts:sign_in')
 
     def post(self, request, *args, **kwargs):
         return super(SignUpView, self).post(request, *args, **kwargs)
 
+    def get_success_url(self):
+        return reverse_lazy('accounts:sign_in')
+
 class SignInView(LoginView):
-    template_name = 'sign_in.html'
+    template_name = 'accounts/sign_in.html'
 
     def get_success_url(self):
         #매칭 앱 url로 연결
