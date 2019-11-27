345e16c15da75cdc5b42a94596ae58b576afed67
minjoong <albert507@naver.com>
2019-11-27 21:22:32 +0900

    make user_message form

---
+++ user_messages/forms.py
@@ -0,0 +1,18 @@
+from django.forms import models
+from user_messages.models import UserMessages
+
+class UserMessageForm(models.ModelForm):
+    class Meta:
+        model = UserMessages
+        fields = ['content']
+
+    def __init__(self, receiver, sender, pet, *args, **kwargs):
+        self.receiver = receiver
+        self.sender = sender
+        self.pet = pet
+        super(UserMessageForm, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        self.instance.receiver = self.receiver
+        self.instance.sender = self.sender
+        return super(UserMessageForm, self).save(commit)
