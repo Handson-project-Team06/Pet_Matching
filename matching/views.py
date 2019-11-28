4d31ff2ea2358e7efd0c391b25ee5218fe76ec6e
윤연경 <amyss0907@gmail.com>
2019-11-28 20:05:58 +0900

    출력확인용

--- matching/views.py
+++ matching/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from matching.models import Pet
-from matching.filters import PetFilter,get_locations_nearby_coords
+from matching.filters import PetFilter,get_locations_nearby_coords,getplace
 # Create your views here.
 
 def home(request):
     pet_list=Pet.objects.all()
     pet_filter = PetFilter(request.GET,queryset=pet_list)
-    nearby_locations , distance = get_locations_nearby_coords(48.8582, 2.2945, 5)
-    return render(request,'home.html',{'filter':pet_filter, 'nearby_locations':nearby_locations, 'distance':distance})
+    nearby_locations , distance = get_locations_nearby_coords(2,5, 7000)
+    town, country = getplace(2.0, 3)
+    return render(request,'home.html',{'filter':pet_filter, 'nearby_locations':nearby_locations, 'distance':distance, 'town' : town, 'country' : country})
 
